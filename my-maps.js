7bd9db77e3eb2fd671e00390f45b414bfc7de5ba
gautiaka <gautiaka@gmail.com>
2018-04-30 15:22:21 +0530

    code beautified

--- my-maps.js
+++ my-maps.js
@@ -2,25 +2,29 @@ var markers = []; //Array of locations
 var map;
 var directionsService;
 var directionsDisplay;
+
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: {lat: 18.5204, lng: 73.8567}
-        });
-        directionsDisplay.setMap(map);
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: {
+      lat: 18.5204,
+      lng: 73.8567
+    }
+  });
+  directionsDisplay.setMap(map);
   map.addListener('click', function(e) {
     placeMarkerAndPanTo(e.latLng, map);
     console.log("marker placed at " + e.latLng);
   });
 
-var onClickRoutesHandler = function() {
-  calculateAndDisplayRoute(map,directionsService, directionsDisplay);
-}
-var onClickLinesHandler = function() {
-  generatePolyLines(map);
-}
+  var onClickRoutesHandler = function() {
+    calculateAndDisplayRoute(map, directionsService, directionsDisplay);
+  }
+  var onClickLinesHandler = function() {
+    generatePolyLines(map);
+  }
   document.getElementById('getRoute').addEventListener('click', onClickRoutesHandler);
   document.getElementById('getPolylines').addEventListener('click', onClickLinesHandler);
 }
@@ -36,99 +40,93 @@ function placeMarkerAndPanTo(latLng, map) {
 }
 
 
-function calculateAndDisplayRoute(map,directionsService, directionsDisplay) {
-var waypts = [];
-var numberOfMarkers = markers.length;
-var myOrigin = markers[0];
-var myDestination = markers[numberOfMarkers - 1 ];
-// console.log(myOrigin);
-// console.log(myDestination);
-// console.log(numberOfMarkers);
-if(numberOfMarkers>2){
-  for (var i = 0; i < markers.length; i++) {
-
-           waypts.push({
-             location: markers[i],
-             stopover: true
-           });
-         }
-}
-     directionsService.route({
-       origin: myOrigin,
-       destination: myDestination,
-       waypoints: waypts ? waypts : 'none',
-                optimizeWaypoints: true,
-       travelMode: 'DRIVING'
-     }, function(response, status) {
-       if (status === 'OK') {
-         directionsDisplay.setDirections(response);
-
-         generatePolyLines(map,response);
-       } else {
-         window.alert('Directions request failed due to ' + status);
-       }
-     });
-   }
-
-function generatePolyLines(map,response){
-  console.log("polulines clicked");
-  console.info(map);
-  console.info(response);
+function calculateAndDisplayRoute(map, directionsService, directionsDisplay) {
+  var waypts = [];
   var numberOfMarkers = markers.length;
   var myOrigin = markers[0];
-  var myDestination = markers[numberOfMarkers - 1 ];
+  var myDestination = markers[numberOfMarkers - 1];
+
+  if (numberOfMarkers > 2) {
+    for (var i = 0; i < markers.length; i++) {
+
+      waypts.push({
+        location: markers[i],
+        stopover: true
+      });
+    }
+  }
+  directionsService.route({
+    origin: myOrigin,
+    destination: myDestination,
+    waypoints: waypts ? waypts : 'none',
+    optimizeWaypoints: true,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+
+      generatePolyLines(map, response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+function generatePolyLines(map, response) {
+
+  var numberOfMarkers = markers.length;
+  var myOrigin = markers[0];
+  var myDestination = markers[numberOfMarkers - 1];
   var lineSymbol = {
-         path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-         scale: 8,
-         strokeColor: '#393'
-       };
-  var flightPlanCoordinates = [
-        myOrigin,
-        myDestination
-       ];
-       var line = new google.maps.Polyline({
-         path: [],
-         geodesic: true,
-         icons: [{
-            icon: lineSymbol,
-            offset: '100%'
-          }],
-         strokeColor: '#FF0000',
-         strokeOpacity: 1.0,
-         strokeWeight: 2
-       });
+    path: google.maps.SymbolPath.CIRCLE,
+    scale: 8,
+    strokeColor: '#393'
+  };
 
-       var bounds = new google.maps.LatLngBounds();
+  var line = new google.maps.Polyline({
+    path: [],
+    geodesic: true,
+    icons: [{
+      icon: lineSymbol,
+      offset: '100%'
+    }],
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+
+  var bounds = new google.maps.LatLngBounds();
 
 
-       var legs = response.routes[0].legs;
-       for (i=0;i<legs.length;i++) {
-         var steps = legs[i].steps;
-         for (j=0;j<steps.length;j++) {
-           var nextSegment = steps[j].path;
-           for (k=0;k<nextSegment.length;k++) {
-             line.getPath().push(nextSegment[k]);
-             bounds.extend(nextSegment[k]);
-           }
-         }
-       }
+  var legs = response.routes[0].legs;
+  for (i = 0; i < legs.length; i++) {
+    var steps = legs[i].steps;
+    for (j = 0; j < steps.length; j++) {
+      var nextSegment = steps[j].path;
+      for (k = 0; k < nextSegment.length; k++) {
+        line.getPath().push(nextSegment[k]);
+        bounds.extend(nextSegment[k]);
+      }
+    }
+  }
 
 
 
 
-       line.setMap(map);
-       map.fitBounds(bounds);
+  line.setMap(map);
+  map.fitBounds(bounds);
 
-        animateCircle(line);
+  animateCircle(line);
 
 }
-function animateCircle(line) {
-          var count = 0;
-          window.setInterval(function() {
-            count = (count + 1) % 200;
 
-            var icons = line.get('icons');
-            icons[0].offset = (count / 2) + '%';
-            line.set('icons', icons);
-        }, 100);
-      }
+function animateCircle(line) {
+  var count = 0;
+  window.setInterval(function() {
+    count = (count + 1) % 200;
+
+    var icons = line.get('icons');
+    icons[0].offset = (count / 2) + '%';
+    line.set('icons', icons);
+  }, 100);
+}
